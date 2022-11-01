e6b300fe14d2a05506b1d64ae92f45cb9863c26f
BrianRid <brianridolce@gmail.com>
2022-11-01 17:57:06 +0100

    installing chakra + verofying user api logic status: ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { Button, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { signIn } from "next-auth/react";
 import React, { useState } from "react";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Heading>Home page</Heading>
+      <Button onClick={() => signIn("google")}>Login with Google</Button>
+      <Button onClick={() => signIn("credentials")}>
+        Login with Credentials
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
